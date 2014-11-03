7ba14ded5953dfea439f4e6ca2ac289e96e09945
Jaime Alonso Lorenzo <jaimealonsolorenzo@gmail.com>
2014-11-03 19:04:34 +0100

    Refactored ClientApp.

--- src/com/santi/jaime/TA/ClientApp.java
+++ src/com/santi/jaime/TA/ClientApp.java
@@ -2,6 +2,7 @@ package com.santi.jaime.TA;
 
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
 import java.util.List;
 import java.util.Scanner;
 
@@ -23,14 +24,21 @@ import com.amazonaws.services.sqs.model.DeleteMessageRequest;
 import com.amazonaws.services.sqs.model.Message;
 import com.amazonaws.services.sqs.model.ReceiveMessageRequest;
 import com.amazonaws.services.sqs.model.SendMessageRequest;
+import com.amazonaws.services.sqs.model.SendMessageResult;
 
 public class ClientApp {
 
-	public static void main(String[] args) throws Exception {
+	private AWSCredentials credentials;
+	private AmazonSQS sqs;
+	private Region usWest2;
+	private AmazonS3 s3client;
+	private String bucketName;
+	
+	public ClientApp(){
 		/*
 		 * The ProfileCredentialsProvider will return your [default] credential profile by reading from the credentials file located at (C:\\Users\\Santi\\.aws\\credentials).
 		 */
-		AWSCredentials credentials = null;
+		credentials = null;
 
 		try {
 			credentials = new ProfileCredentialsProvider("default").getCredentials();
@@ -38,43 +46,19 @@ public class ClientApp {
 			throw new AmazonClientException(e.getMessage());
 		}
 
-		AmazonSQS sqs = new AmazonSQSClient(credentials);
-		Region usWest2 = Region.getRegion(Regions.EU_WEST_1);
+		sqs = new AmazonSQSClient(credentials);
+		usWest2 = Region.getRegion(Regions.EU_WEST_1);
 		sqs.setRegion(usWest2);
-		AmazonS3 s3client = new AmazonS3Client(new ProfileCredentialsProvider());
-		String bucketName = "g3-bucket-2";
+		s3client = new AmazonS3Client(new ProfileCredentialsProvider());
+		bucketName = "g3-bucket-2";
+	}
+	
+	public static void main(String[] args) throws Exception {
+		
+		ClientApp thisOne = new ClientApp();
 
-		// Now we look for our queue inbox
-
-		// We attemp to receive the request, first of all we check if the outbox queue is already created
-		boolean existsIn = false, existsOut = false;
-		String sqsInbox = "";
-		String sqsOutbox = "";
-		for (String queueUrl : sqs.listQueues().getQueueUrls()) {
-			String [] pieces = queueUrl.split("/");
-			if (pieces[pieces.length-1].equals("g3-inbox")) {
-				existsIn = true;
-				sqsInbox = queueUrl;
-				System.out.println("Inbox queue fetched");
-			}
-			else if (pieces[pieces.length-1].equals("g3-outbox")) {
-				existsOut = true;
-				sqsOutbox = queueUrl;
-				System.out.println("Outbox queue fetched");
-			}
-		}
-		if (!existsIn) {
-			// It doesn't exist so we create it
-			CreateQueueRequest createQueueRequest = new CreateQueueRequest("g3-inbox");
-			sqsInbox = sqs.createQueue(createQueueRequest).getQueueUrl();
-			System.out.println("Queue created, beginning work!");
-		}
-		if (!existsOut) {
-			// It doesn't exist so we create it
-			CreateQueueRequest createQueueRequest = new CreateQueueRequest("g3-outbox");
-			sqsInbox = sqs.createQueue(createQueueRequest).getQueueUrl();
-			System.out.println("Queue created, beginning work!");
-		}
+		String sqsInbox = thisOne.getQueueUrl("g3-inbox");
+		String sqsOutbox = thisOne.getQueueUrl("g3-outbox");;
 
 		String input = "";
 
@@ -89,16 +73,44 @@ public class ClientApp {
 			throw new Exception("Image not found.");
 		}
 
-		if (!s3client.doesBucketExist(bucketName)) {
-			s3client.createBucket(bucketName, com.amazonaws.services.s3.model.Region.EU_Ireland);
+		thisOne.putObjectInBucket(input_lowercase, photoFile);
+		
+		thisOne.sendMessage(sqsInbox, input_lowercase);
+
+		String fileKey = thisOne.receiveMessage(sqsOutbox);
+		S3ObjectInputStream fileContent = thisOne.getObjectFromBucket(fileKey);
+		BufferedImage imageFetched = ImageIO.read(fileContent);
+		File outPutImage = new File(fileKey + ".jpg");
+		ImageIO.write(imageFetched, "jpg", outPutImage);
+		System.out.println("All jobs finished");
+	}
+	
+	public String getQueueUrl(String queueName){
+		boolean exists = false;
+		String queue = "";
+		for (String queueUrl : sqs.listQueues().getQueueUrls()) {
+			String [] pieces = queueUrl.split("/");
+			if (pieces[pieces.length-1].equals(queueName)) {
+				exists = true;
+				queue = queueUrl;
+				System.out.println(queueName+" queue found");
+				break;
+			}
+		}
+		if (!exists) {
+			// It doesn't exist so we create it
+			CreateQueueRequest createQueueRequest = new CreateQueueRequest(queueName);
+			queue = sqs.createQueue(createQueueRequest).getQueueUrl();
+			System.out.println("Queue created");
 		}
 
-		s3client.putObject(bucketName, input_lowercase, photoFile);
-
-		sqs.sendMessage(new SendMessageRequest(sqsInbox, input_lowercase));
-
-		// Now we try to fetch the response
-		ReceiveMessageRequest receiveMessageRequest = new ReceiveMessageRequest(sqsOutbox);
+		return queue;
+	}
+	
+	
+	public String receiveMessage(String queue){
+		String body = null;
+		ReceiveMessageRequest receiveMessageRequest = new ReceiveMessageRequest(queue);
 		int message_number = 0;
 		List<Message> messages = null;
 		do {
@@ -108,15 +120,37 @@ public class ClientApp {
 
 		// As soon as we get the message we delete it so that the other queue doesn't get it
 		String messageRecieptHandle = messages.get(0).getReceiptHandle();
-		sqs.deleteMessage(new DeleteMessageRequest(sqsOutbox, messageRecieptHandle));
+		sqs.deleteMessage(new DeleteMessageRequest(queue, messageRecieptHandle));
+		
+		body = messages.get(0).getBody();
+		
+		return body;
+		
+	}
+	
+	public int sendMessage(String queueUrl, String body){
+		SendMessageRequest message_out = new SendMessageRequest(queueUrl, body);
+		sqs.sendMessage(message_out);
+		
+		return 0;
+	}
+	
+	public int putObjectInBucket(String key, File bucketFile) throws Exception{
 
-		String fileKey = messages.get(0).getBody();
-		S3Object file = s3client.getObject(bucketName, fileKey);
+		if (!s3client.doesBucketExist(bucketName)) {
+			s3client.createBucket(bucketName, com.amazonaws.services.s3.model.Region.EU_Ireland);
+		}
+
+		s3client.putObject(bucketName, key, bucketFile);
+		
+		return 0;
+	}
+	
+	public S3ObjectInputStream getObjectFromBucket(String keyObject) throws Exception{
+		S3Object file = s3client.getObject(bucketName, keyObject);
 		S3ObjectInputStream fileContent = file.getObjectContent();
-		BufferedImage imageFetched = ImageIO.read(fileContent);
-		File outPutImage = new File(fileKey + ".jpg");
-		ImageIO.write(imageFetched, "jpg", outPutImage);
-		System.out.println("All jobs finished");
+		
+		return fileContent;
 	}
 
 }
--- src/com/santi/jaime/TA/ServletApp.java
+++ src/com/santi/jaime/TA/ServletApp.java
@@ -25,8 +25,29 @@ public class ServletApp extends HttpServlet{
 		pw.println("<h1>Welcome to our Assignment 2 Servlet!</h1><br /><br />");
 		pw.println("<h3>Please select one the following options</h3>");
 		
+		pw.println("<form action='ServletApp' method='post' name='choice'>");
+
+		pw.println("<input type='radio' name='city' value='Cracovia'>Cracovia<br/>");
+		pw.println("<input type='radio' name='city' value='Delft'>Delft<br/>");
+		pw.println("<input type='radio' name='city' value='Pontevedra'>Pontevedra<br/>");
+		pw.println("<input type='radio' name='city' value='Vigo'>Vigo<br/>");
+		
+		pw.println("</form>");
 		pw.println("</body></html>");
 		
+		
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		PrintWriter pw = resp.getWriter();
+		resp.setContentType("text/html");
+		pw.println("<html>");
+		pw.println("<head>");
+		pw.println("<title>TA Assignment 2 Servlet Service</title>");
+		pw.println("</head><body bgcolor=\"yellow\">");
+		pw.println("<h1>Welcome to our Assignment 2 Servlet!</h1><br /><br />");
 		
 	}
 }
